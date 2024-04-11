4b2b1101605538a476df51ea76d88959d6af79c1
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-11 17:04:43 +0600

    React-Alias Configuation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from '@/pages/HomePage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={} />
+        <Route element={<HomePage />} />
       </Routes>
       <h1>React Redux - Redux Toolkit</h1>
     </BrowserRouter>

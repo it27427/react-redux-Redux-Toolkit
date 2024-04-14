c669a5262425c5d364af03e28f54439a17d1799f
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-14 10:26:34 +0600

    Stats value updated

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,17 @@
 import Counter from '@/components/Counter';
+import Stats from '@/components/Stats';
 
 const HomePage = () => {
   return (
-    <div className='w-screen h-screen p-10 bg-gray-100 text-slate-700'>
+    <div className='min-h-screen p-10 bg-gray-100 text-slate-700'>
       <h1 className='max-w-md mx-auto text-center text-2xl font-bold'>
         Counter Application
       </h1>
 
       <div className='max-w-md mx-auto mt-10 space-y-5'>
         <Counter />
+        <Counter />
+        <Stats totalCount={10} />
       </div>
     </div>
   );

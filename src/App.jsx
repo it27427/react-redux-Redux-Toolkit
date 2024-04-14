5e739d44c1cf6efc1e5aacb4e4faec52433fd589
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-15 00:06:47 +0600

    blogs page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import HomePage from '@/pages/HomePage';
+import Blogspage from '@/pages/Blogspage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/blogs' element={<Blogspage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Blogspage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogspage = () => {
+  return (
+    <div>
+      <h1>Blogs Page</h1>
+    </div>
+  );
+};
+
+export default Blogspage;

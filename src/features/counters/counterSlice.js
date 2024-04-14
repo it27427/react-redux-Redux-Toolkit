50f22645861f185d5e9be2c6142cdd77b243e17a
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-14 11:59:22 +0600

    counterSlice

--- src/features/counters/counterSlice.js
+++ src/features/counters/counterSlice.js
@@ -1,3 +1,23 @@
 const { createSlice } = require('@reduxjs/toolkit');
 
-const counterSlice = createSlice();
+const initialState = [
+  { id: 1, value: 0 },
+  { id: 2, value: 0 },
+];
+
+const counterSlice = createSlice({
+  name: 'counters',
+  initialState,
+  reducers: {
+    increment: (state, action) => {
+      const counterIndex = state.findIndex((c) => c.id === action.payload);
+      state[counterIndex].value++;
+    },
+    decrement: (state, action) => {
+      const counterIndex = state.findIndex((c) => c.id === action.payload);
+      state[counterIndex].value--;
+    },
+  },
+});
+
+export default counterSlice.reducer;

3a1d614a2f829b7268c108da9d1e0b99aeca97fa
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-15 00:13:25 +0600

    blog posts

--- src/pages/Blogspage.jsx
+++ src/pages/Blogspage.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts } from '@/features/posts/postSlice';
 
 const Blogspage = () => {
+  const posts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPosts);
+  }, []);
+
   return (
-    <div>
-      <h1>Blogs Page</h1>
+    <div className='min-h-screen p-10 bg-gray-100 text-slate-700'>
+      <h2 className='max-w-md mx-auto text-center text-2xl font-bold'>Blogs</h2>
+
+      <div className='max-w-md mx-auto mt-10 space-y-5'></div>
     </div>
   );
 };

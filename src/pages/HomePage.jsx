4c41713e4962b2e2e1367bfd4aef34c9ef4af667
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-14 10:47:54 +0600

    handling stats

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,22 @@
 import Counter from '@/components/Counter';
 import Stats from '@/components/Stats';
 
+const initialCounters = [
+  { id: 1, value: 0 },
+  { id: 2, value: 0 },
+];
+
 const HomePage = () => {
+  const [counters, setCounters] = useState(initialCounters);
+
+  const handleIncrement = (counterId) => {
+    setCounters((prevCount) => prevCount + 1);
+  };
+
+  const handleDecrement = (counterId) => {
+    setCounters((prevCount) => prevCount - 1);
+  };
+
   return (
     <div className='min-h-screen p-10 bg-gray-100 text-slate-700'>
       <h1 className='max-w-md mx-auto text-center text-2xl font-bold'>
@@ -9,8 +24,10 @@ const HomePage = () => {
       </h1>
 
       <div className='max-w-md mx-auto mt-10 space-y-5'>
-        <Counter />
-        <Counter />
+        {counters.map((counter) => (
+          <Counter key={counter.id} />
+        ))}
+
         <Stats totalCount={10} />
       </div>
     </div>
